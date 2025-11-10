751d1eb9ddec381c425cb6e697a2d9f4c80c63e6
mvdevelop <marcosvmdilly@gmail.com>
2025-11-10 11:55:35 -0300

    working on nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,6 @@
 
 import React, { useState } from 'react';
 
-
-
 export default function Nav() {
 
   const [ menuOpen, setMenuOpen ] = useState(false);
@@ -19,12 +17,16 @@ export default function Nav() {
     <>
       <nav className='w-full flex flex-col justify-center items-center relative'>
         {/* Top Bar */}
-        <div className="top-nav w-full flex justify-between items-center bg-black text-white px-[8%] lg:px-[12%] py-3 text-sm">
-          <div className='flex w-1/2 gap-5 items-center'>
+        <div className="top-nav w-full flex justify-around items-center bg-black text-white px-[8%] lg:px-[12%] py-3 text-sm">
+          <div className='flex w-3/4 gap-5 items-center'>
             <div className='relative group'>
-              <span className='cursor-pointer flex items-center hover:text-blue-600'>
+              <span className='cursor-pointer flex items-center hover:text-blue-300'>
                 English <span className='ml-1 text-xs'>▼</span>
               </span>
+              <ul className='absolute top-full left-0 bg-white text-black shadow-md rounded-md p-4 hidden group-hover:flex flex-col gap-2 z-50'>
+                <li>French</li>
+                <li>Deutsch</li>
+              </ul>
             </div>
           </div>
         </div>
